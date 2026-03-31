39cfa8381a5ff65ca82cecc8fdbfcc386f94ec7b
DanielTurner-1 <W0513528@nscc.ca>
2026-03-31 08:53:53 -0300

    Itemcontroller created, and item related routes are added

--- server/routes.cjs
+++ server/routes.cjs
@@ -1,5 +1,6 @@
 const express = require('express')
 const CategoryController = require('./controllers/CategoryController.cjs')
+const ItemController = require('./controllers/ItemController.cjs')
 
 const router = express.Router()
 
@@ -8,5 +9,11 @@ router.get('/categories', CategoryController.index)
 router.post('/categories', CategoryController.store)
 router.patch('/categories/:category', CategoryController.update)
 router.delete('/categories/:category', CategoryController.destroy)
+
+router.get('/items', ItemController.index)
+router.post('/items', ItemController.store)
+router.patch('/items/:item', ItemController.update)
+router.delete('/items/:item', ItemController.destroy)
+
 
 module.exports = router
