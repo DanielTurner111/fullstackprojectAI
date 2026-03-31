399813efdc3dbd8a8b7c9917961b0cf31f78a6aa
DanielTurner-1 <W0513528@nscc.ca>
2026-03-31 10:34:15 -0300

    Form code written, still need to make category a drop down

---
+++ src/components/FormComponents/AddForm/AddItemForm.jsx
@@ -0,0 +1,96 @@
+import './AddItemForm.scss'
+
+import Button from '../Button/Button.jsx'
+import { useState, useEffect, use } from 'react'
+
+const AddItemForm = props => {
+
+    const [category_id, setCategoryID] = useState('')
+    const [title, setTitle] = useState('')
+    const [description, setDesc] = useState('')
+    const [price, setPrice] = useState('')
+    const [quantity, setQuantity] = useState('')
+    const [sku, setSku] = useState('')
+
+
+    const [entry, setEntry] = useState(false)
+
+    const _add = () => {
+        props.onAddItem(entry)
+        _clear()
+    }
+
+    const _clear = () => {
+        setCategoryID('')
+        setTitle('')
+        setDesc('')
+        setPrice('')
+        setQuantity('')
+        setSku('')
+    }
+
+    useEffect(() => {
+        console.log('entry changed')
+        console.log(`entry: ${JSON.stringify(entry)}`)
+
+        setButtonState( (entry.category_id === '', entry.title === '', entry.description === '', entry.price === '', entry.quantity === '', entry.sku === '') ? false : true ) 
+
+    },[entry])
+
+    useEffect(() => {
+        setEntry({ 'category_id' : category_id, 'title' : title, 'description' : description, 'price' : price, 'quantity' : quantity, 'sku' : sku})
+    },[category_id, title, description, price, quantity, sku])
+
+        const _detectValueChanged = (key, value) => {
+        if (key === 'category_id') {
+            setCategoryID(value)
+        } else if (key === 'title'){
+            setTitle(value)
+        }else if (key === 'description'){
+            setDesc(value)
+        }else if (key === 'price'){
+            setPrice(value)
+        }else if (key === 'quantity'){
+            setDesc(value)
+        }else if (key === 'key'){
+            setDesc(value)
+        }
+        console.log('_detectValueChanged triggered')
+    }
+
+    return(
+        <div className='ItemForm'>
+            <Button clickme={ _add } title='Add Entry' enabled={ buttonState }/>
+            <br/>
+            <label>Category ID:</label>
+            <input type='text' placeholder='Category_id' value={category_id}
+                   onChange = { e => _detectValueChanged('category_id', e.target.value) } />
+            <br/>
+            <label>Title</label>
+            <input type='text' placeholder='Title' value={title}
+                   onChange = { e => _detectValueChanged('title', e.target.value) } />
+            <br/>
+             <label>Description</label>
+            <input type='text' placeholder='Description' value={description}
+                   onChange = { e => _detectValueChanged('title', e.target.value) } />
+            <br/>
+            <label>Price</label>
+            <input type='text' placeholder='Price' value={price}
+                   onChange = { e => _detectValueChanged('price', e.target.value) } />
+            <br/>
+            <label>Quantity</label>
+            <input type='text' placeholder='Quantity' value={quantity}
+                   onChange = { e => _detectValueChanged('quantity', e.target.value) } />
+            <br/>
+            <label>Sku</label>
+            <input type='text' placeholder='Sku' value={sku}
+                   onChange = { e => _detectValueChanged('sku', e.target.value) } />
+            <br/>
+            
+            
+        </div>
+    )
+
+}
+
+export default AddItemForm
---
+++ src/components/FormComponents/EditForm/EditItemForm.jsx
@@ -0,0 +1,113 @@
+import './EditItemForm.scss'
+
+import Button from '../Button/Button.jsx'
+import { useState, useEffect } from 'react'
+
+const EditItemForm = props => {
+
+
+    const _update = () => {
+        props.onUpdateItem(entry)
+        _clear()
+    }
+
+    const [id, setID] = useState('')
+    const [category_id, setCategoryID] = useState('')
+    const [title, setTitle] = useState('')
+    const [description, setDesc] = useState('')
+    const [price, setPrice] = useState('')
+    const [quantity, setQuantity] = useState('')
+    const [sku, setSku] = useState('')
+
+    const [entry, setEntry] = useState({})
+
+    const [buttonState, setButtonState] = useState(false)
+
+    const _clear = () => {
+        setCategoryID('')
+        setTitle('')
+        setDesc('')
+        setPrice('')
+        setQuantity('')
+        setSku('')
+    }
+
+    useEffect(() => {
+        console.log('category changed')
+        console.log(`category: ${JSON.stringify(entry)}`)
+
+
+        setButtonState( (entry.category_id === '' || entry.title === '' || entry.description === '' || entry.price === '' || entry.quantity === '' || entry.sku === '') ? false : true ) 
+
+    },[entry])
+
+    useEffect(() => {
+        setEntry({ 'id': id, 'category_id' : category_id, 'title' : title, 'description' : description, 'price' : price, 'quantity' : quantity, 'sku' : sku})
+    },[id, category_id, title, description, price, quantity, sku])
+
+     const _detectValueChanged = (key, value) => {
+        if (key === 'category_id') {
+            setCategoryID(value)
+        } else if (key === 'title'){
+            setTitle(value)
+        }else if (key === 'description'){
+            setDesc(value)
+        }else if (key === 'price'){
+            setPrice(value)
+        }else if (key === 'quantity'){
+            setDesc(value)
+        }else if (key === 'key'){
+            setDesc(value)
+        }
+        console.log('_detectValueChanged triggered')
+    }
+
+    useEffect(() =>{
+        if (props.item){
+            setID(props.item.id)
+            setCategoryID(props.item.category_id)
+            setTitle(props.item.title)
+            setDesc(props.item.description)
+            setPrice(props.item.price)
+            setQuantity(props.item.quantity)
+            setSku(props.item.sku)
+        }
+
+    }, [props])
+
+    return(
+        <div className='EditItemForm'>
+            <Button clickme={ _update } title='Edit Entry' enabled={ buttonState }/>
+            <br/>
+            <label>Category ID:</label>
+            <input type='text' placeholder='Category_id' value={category_id}
+                   onChange = { e => _detectValueChanged('category_id', e.target.value) } />
+            <br/>
+            <label>Title</label>
+            <input type='text' placeholder='Title' value={title}
+                   onChange = { e => _detectValueChanged('title', e.target.value) } />
+            <br/>
+             <label>Description</label>
+            <input type='text' placeholder='Description' value={description}
+                   onChange = { e => _detectValueChanged('title', e.target.value) } />
+            <br/>
+            <label>Price</label>
+            <input type='text' placeholder='Price' value={price}
+                   onChange = { e => _detectValueChanged('price', e.target.value) } />
+            <br/>
+            <label>Quantity</label>
+            <input type='text' placeholder='Quantity' value={quantity}
+                   onChange = { e => _detectValueChanged('quantity', e.target.value) } />
+            <br/>
+            <label>Sku</label>
+            <input type='text' placeholder='Sku' value={sku}
+                   onChange = { e => _detectValueChanged('sku', e.target.value) } />
+            <br/>
+            
+            
+        </div>
+    )
+
+}
+
+export default EditItemForm
